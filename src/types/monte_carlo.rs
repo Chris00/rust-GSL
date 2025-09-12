12131ef0aa6244862fedad72cd9562cc7463e6a5
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2025-09-12 19:07:00 +0200

    Slightly improve the documentation of types::monte_carlo

--- src/types/monte_carlo.rs
+++ src/types/monte_carlo.rs
@@ -9,9 +9,9 @@ This chapter describes routines for multidimensional Monte Carlo integration. Th
 algorithms such as VEGAS and MISER which use importance sampling and stratified sampling techniques. Each algorithm computes an estimate of
 a multidimensional definite integral of the form,
 
-I = \int_xl^xu dx \int_yl^yu  dy ...  f(x, y, ...)
+$$I = \int_{x_l}^{x_u} dx \int_{y_l}^{y_u} dy ...  f(x, y, ...)$$
 
-over a hypercubic region ((x_l,x_u), (y_l,y_u), ...) using a fixed number of function calls. The routines also provide a statistical estimate
+over a hypercubic region $\[x_l,x_u\] × \[y_l,y_u\] × ⋯$ using a fixed number of function calls. The routines also provide a statistical estimate
 of the error on the result. This error estimate should be taken as a guide rather than as a strict error bound—random sampling of the region
 may not uncover all the important features of the function, resulting in an underestimate of the error.
 
@@ -30,23 +30,26 @@ are automatically avoided.
 ## VEGAS
 
 The VEGAS algorithm of Lepage is based on importance sampling. It samples points from the probability distribution described by the function
-|f|, so that the points are concentrated in the regions that make the largest contribution to the integral.
+$|f|$, so that the points are concentrated in the regions that make the largest contribution to the integral.
 
-In general, if the Monte Carlo integral of f is sampled with points distributed according to a probability distribution described by the function
-g, we obtain an estimate E_g(f; N),
+In general, if the Monte Carlo integral of $f$ is sampled with points
+distributed according to a probability distribution described by the
+function $g$, we obtain an estimate $E_g(f; N)$,
 
-E_g(f; N) = E(f/g; N)
+$$E_g(f; N) = E(f/g; N)$$
+
 with a corresponding variance,
 
-\Var_g(f; N) = \Var(f/g; N).
-If the probability distribution is chosen as g = |f|/I(|f|) then it can be shown that the variance V_g(f; N) vanishes, and the error in the
+$$\Var_g(f; N) = \Var(f/g; N).$$
+
+If the probability distribution is chosen as $g = |f|/I(|f|)$ then it can be shown that the variance $V_g(f; N)$ vanishes, and the error in the
 estimate will be zero. In practice it is not possible to sample from the exact distribution g for an arbitrary function, so importance sampling
 algorithms aim to produce efficient approximations to the desired distribution.
 
 The VEGAS algorithm approximates the exact distribution by making a number of passes over the integration region while histogramming the
 function f. Each histogram is used to define a sampling distribution for the next pass. Asymptotically this procedure converges to the desired
 distribution. In order to avoid the number of histogram bins growing like K^d the probability distribution is approximated by a separable
-function: g(x_1, x_2, ...) = g_1(x_1) g_2(x_2) ... so that the number of bins required is only Kd. This is equivalent to locating the
+function: $g(x_1, x_2, ...) = g_1(x_1) g_2(x_2) ...$ so that the number of bins required is only $Kd$. This is equivalent to locating the
 peaks of the function from the projections of the integrand onto the coordinate axes. The efficiency of VEGAS depends on the validity of
 this assumption. It is most efficient when the peaks of the integrand are well-localized. If an integrand can be rewritten in a form which
 is approximately separable this will increase the efficiency of integration with VEGAS.
@@ -60,7 +63,7 @@ The VEGAS algorithm computes a number of independent estimates of the integral i
 below, and returns their weighted average. Random sampling of the integrand can occasionally produce an estimate where the error is zero,
 particularly if the function is constant in some regions. An estimate with zero error causes the weighted average to break down and must
 be handled separately. In the original Fortran implementations of VEGAS the error estimate is made non-zero by substituting a small value
-(typically 1e-30). The implementation in GSL differs from this and avoids the use of an arbitrary constant—it either assigns the value a
+(typically `1e-30`). The implementation in GSL differs from this and avoids the use of an arbitrary constant—it either assigns the value a
 weight which is the average weight of the preceding estimates or discards it according to the following procedure,
 
 current estimate has zero error, weighted average has finite error
@@ -71,7 +74,7 @@ The previous estimates are discarded and the weighted averaging procedure begins
 
 current estimate has zero error, previous estimates had zero error
 The estimates are averaged using the arithmetic mean, but no error is computed.
-!*/
+*/
 
 use crate::ffi::FFI;
 use crate::Error;
@@ -85,16 +88,16 @@ ffi_wrapper!(
     /// Using this algorithm the estimate of the integral $E(f;N)$ for
     /// $N$ randomly distributed points $x_i$ is given by,
     ///
-    /// $$E(f; N) =  V<f> = (V / N) \sum_i^N f(x_i)$$
+    /// $$E(f; N) =  V⟨f⟩ = (V / N) \sum_i^N f(x_i)$$
     ///
     /// where $V$ is the volume of the integration region.  The error
     /// on this estimate $\sigma(E;N)$ is calculated from the
     /// estimated variance of the mean,
     ///
-    /// $$\sigma^2 (E; N) = (V^2 / N^2) sum_i^N (f(x_i) -  <f>)^2.$$
+    /// $$\sigma^2 (E; N) = (V^2 / N^2) \sum_i^N (f(x_i) -  ⟨f⟩)^2.$$
     ///
     /// For large $N$ this variance decreases asymptotically as
-    /// $Var(f)/N$, where $Var(f)$ is the true variance of the
+    /// $\Var(f)/N$, where $\Var(f)$ is the true variance of the
     /// function over the integration region.  The error estimate
     /// itself should decrease as $\sigma(f)/\sqrt{N}$.  The familiar
     /// law of errors decreasing as $1/sqrt{N}$ applies-to reduce the
