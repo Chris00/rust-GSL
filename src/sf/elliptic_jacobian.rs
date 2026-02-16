0e602faed95cd7d559056e03cc46083f78ee8f5d
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2026-02-16 11:25:57 +0100

    Improve the interface of the module  elliptic_jacobian

--- src/sf/elliptic_jacobian.rs
+++ src/sf/elliptic_jacobian.rs
@@ -1,22 +1,50 @@
-//
-// A rust binding for the GSL library by Guillaume Gomez (guillaume1.gomez@gmail.com)
-//
-
-/*!
-The Jacobian Elliptic functions are defined in Abramowitz & Stegun, Chapter 16.
-!*/
+//! The Jacobian Elliptic functions are defined in Abramowitz &
+//! Stegun, Chapter 16.
 
 use crate::Error;
 
-/// This function computes the Jacobian elliptic functions sn(u|m), cn(u|m), dn(u|m) by descending
-/// Landen transformations.
+/// The values of the Jacobian elliptic functions $\sn(u|m)$,
+/// $\cn(u|m)$, and $\dn(u|m)$.
+#[derive(Clone, Copy, Debug)]
+pub struct EllJac {
+    pub sn: f64,
+    pub cn: f64,
+    pub dn: f64,
+}
+
+/// Return the values of the Jacobian elliptic functions $\sn(u|m)$,
+/// $\cn(u|m)$, $\dn(u|m)$.
 ///
-/// Returns `(sn, cn, dn)`.
+/// It uses descending Landen transformations.
 #[doc(alias = "gsl_sf_elljac_e")]
-pub fn elljac_e(u: f64, m: f64) -> Result<(f64, f64, f64), Error> {
+pub fn elljac_e(u: f64, m: f64) -> Result<EllJac, Error> {
     let mut sn = 0.;
     let mut cn = 0.;
     let mut dn = 0.;
     let ret = unsafe { sys::gsl_sf_elljac_e(u, m, &mut sn, &mut cn, &mut dn) };
-    Error::handle(ret, (sn, cn, dn))
+    Error::handle(ret, EllJac { sn, cn, dn })
+}
+
+/// Return $\sn(u|m)$.
+///
+/// This is a convenience function.  It you need to compute other
+/// Jacobian elliptic functions for the same `(u, m)`, use [`elljac_e`].
+pub fn sn(u: f64, m: f64) -> Result<f64, Error> {
+    elljac_e(u, m).map(|e| e.sn)
+}
+
+/// Return $\cn(u|m)$.
+///
+/// This is a convenience function.  It you need to compute other
+/// Jacobian elliptic functions for the same `(u, m)`, use [`elljac_e`].
+pub fn cn(u: f64, m: f64) -> Result<f64, Error> {
+    elljac_e(u, m).map(|e| e.cn)
+}
+
+/// Return $\dn(u|m)$.
+///
+/// This is a convenience function.  It you need to compute other
+/// Jacobian elliptic functions for the same `(u, m)`, use [`elljac_e`].
+pub fn dn(u: f64, m: f64) -> Result<f64, Error> {
+    elljac_e(u, m).map(|e| e.dn)
 }
